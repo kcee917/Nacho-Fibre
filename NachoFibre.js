1f4c7e9a1c197a715ed1550061c633f31dc19949
kcee917 <kcee917@gmail.com>
2021-03-22 19:23:18 -0400

    commit

--- NachoFibre.js
+++ NachoFibre.js
@@ -1,9 +1,18 @@
 
 class NachoFibre{
+// Declare variables
+const fibonacci = [0, 1, 1, 2, 3, 5, 8]
 
-    fibonacci(n){
-        return -1;
+   
+fibonacci(n){
+        NachoFibre [2,3,4,5,6] {;
+             let newNachoFibre = fibonacci(n-1) + fibonacci(n-2);
+        return newNachoFibre;
     }
+}
+
+ 
+
 
     kaleb(n){
         return -1;
@@ -12,3 +21,8 @@ class NachoFibre{
 }
 
 module.exports = NachoFibre;
+
+
+//* *fibonacci(0) = 0*
+//* *fibonacci(1) = 1*
+//* *fibonacci(n) = fibonacci(n-1) + fibonacci(n-2)*
